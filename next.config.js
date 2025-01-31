89f4c6f3fccbec967f9d0f52e0eed15441a4caee
Nguyễn Thái Son Ny <134358623+nguyenthaisonny@users.noreply.github.com>
2025-01-31 18:21:46 +1100

    remove import option at next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // This is the correct way to set output to "export"
   images: {
     unoptimized: true, // Unoptimized images are necessary for static exports
   },
