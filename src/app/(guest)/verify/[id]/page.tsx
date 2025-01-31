db47dc4f9647b98114174b584eac220400943213
Nguyễn Thái Son Ny <134358623+nguyenthaisonny@users.noreply.github.com>
2025-01-31 19:04:14 +1100

    update verify page

--- src/app/(guest)/verify/[id]/page.tsx
+++ src/app/(guest)/verify/[id]/page.tsx
@@ -2,6 +2,7 @@ import Verify from "@/components/auth/verify";
 
 const VerifyPage = ({params} : {params: { id: string }}) => {
     const {id} = params
+    if(!id) return null
     return (
         <Verify id={id}/>
     )
