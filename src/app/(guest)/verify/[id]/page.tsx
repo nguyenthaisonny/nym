31765c5128c701efc539d815bdb5d54f1ed2ba7f
Nguyễn Thái Son Ny <134358623+nguyenthaisonny@users.noreply.github.com>
2025-01-31 18:46:31 +1100

    updatee

--- src/app/(guest)/verify/[id]/page.tsx
+++ src/app/(guest)/verify/[id]/page.tsx
@@ -3,7 +3,7 @@ import Verify from "@/components/auth/verify";
 const VerifyPage = ({params} : {params: { id: string }}) => {
     const {id} = params
     return (
-        <Verify id={id}/>
+        <Verify id={id ?? "1"}/>
     )
 }
 
